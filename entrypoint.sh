f1072002f427908eec506cdcfb206a3ac3154b2b
TaprootFreak <142087526+TaprootFreak@users.noreply.github.com>
2026-04-22 19:57:19 +0200

    Fix Dockerfile for standalone repo (no monorepo paths)

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Replace NEXT_PUBLIC_ environment variables at runtime
 # This allows the same Docker image to be used in DEV and PRD
-find /app/apps/wallet/.next -name '*.js' -exec sed -i'' \
+find /app/.next -name '*.js' -exec sed -i'' \
   -e "s|NEXT_PUBLIC_API_URL|${NEXT_PUBLIC_API_URL}|g" \
   -e "s|NEXT_PUBLIC_EXPLORER_URL|${NEXT_PUBLIC_EXPLORER_URL}|g" \
   -e "s|NEXT_PUBLIC_NETWORK|${NEXT_PUBLIC_NETWORK}|g" \
